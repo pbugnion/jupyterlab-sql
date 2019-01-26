ee17e8526553e8cac2da8480b2df778da2db3699
Pascal Bugnion <pascal@bugnion.org>
2019-01-26 16:02:32 +0000

    Basic structure for frontend app

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,17 @@ import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
 
-import { Widget } from '@phosphor/widgets';
+import {
+  ICommandPalette
+} from '@jupyterlab/apputils';
+
+import {
+  Widget
+} from '@phosphor/widgets';
+
+import {
+  Message
+} from '@phosphor/messaging';
 
 import '../style/index.css';
 
@@ -10,13 +20,43 @@ import '../style/index.css';
 class JupyterLabSqlWidget extends Widget {
   constructor() {
     super();
-    console.log('starting jupyterlab sql extension');
+
+    this.id = "jupyterlab-sql";
+    this.title.label = "SQL";
+    this.title.closable = true;
+    this.elem = document.createElement("div");
+    this.node.appendChild(this.elem);
+  }
+
+  readonly elem: HTMLElement
+
+  onUpdateRequest(message: Message): void {
+    fetch("/jupyterlab_sql")
+      .then(response => response.json())
+      .then(data => {
+        const { result } = data;
+        this.elem.innerHTML = result;
+      })
   }
 }
 
 
-function activate(app: JupyterLab) {
-  new JupyterLabSqlWidget()
+function activate(app: JupyterLab, palette: ICommandPalette) {
+  const widget: JupyterLabSqlWidget = new JupyterLabSqlWidget()
+
+  const command: string = "jupyterlab-sql:open";
+  app.commands.addCommand(command, {
+    label: "SQL",
+    execute: () => {
+      if (!widget.isAttached) {
+        app.shell.addToMainArea(widget);
+      }
+      widget.update();
+      app.shell.activateById(widget.id);
+    }
+  })
+
+  palette.addItem({ command, category: "SQL" });
 }
 
 
@@ -26,6 +66,7 @@ function activate(app: JupyterLab) {
 const extension: JupyterLabPlugin<void> = {
   id: 'jupyterlab-sql',
   autoStart: true,
+  requires: [ICommandPalette],
   activate,
 };
 
