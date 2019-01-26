a2d0951fd37db3381656706c2eeab5dbf1425efa
Pascal Bugnion <pascal@bugnion.org>
2019-01-26 14:58:31 +0000

    Stub for server extension

---
+++ jupyterlab_sql/__init__.py
@@ -0,0 +1,9 @@
+
+def _jupyter_server_extension_paths():
+    return [{
+        "module": "jupyterlab_sql"
+    }]
+
+
+def load_jupyter_server_extension(nbapp):
+    nbapp.log.info("Loading server extension jupyterlab_sql")
---
+++ setup.py
@@ -0,0 +1,11 @@
+
+import setuptools
+
+setuptools.setup(
+    name="jupyterlab_sql",
+    version="0.1.0",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "jupyterlab"
+    ]
+)
