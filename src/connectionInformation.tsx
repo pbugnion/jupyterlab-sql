63f8972077735b2c5b75c81a4a13877b0f562140
Pascal Bugnion <pascal@bugnion.org>
2019-01-31 07:42:40 +0000

    Embed editing state into ConnectionInformation

--- src/connectionInformation.tsx
+++ src/connectionInformation.tsx
@@ -9,6 +9,10 @@ interface ConnectionInformationProps {
   connectionString: string
 }
 
+interface ConnectionInformationState {
+  editing: boolean
+}
+
 export class ConnectionInformationModel extends VDomModel {
   constructor() {
     super()
@@ -29,16 +33,35 @@ export class ConnectionInformationContainer extends VDomRenderer<ConnectionInfor
   }
 }
 
-export class ConnectionInformation extends React.Component<ConnectionInformationProps, {}> {
-  render(): React.ReactElement<any> {
+export class ConnectionInformation extends React.Component<ConnectionInformationProps, ConnectionInformationState> {
+
+  constructor(props: ConnectionInformationProps) {
+    super(props);
+    this.state = { editing: false };
+  }
+
+  startEditing() {
+    this.setState({ editing: true });
+  }
+
+  renderDisplaying() {
     const { connectionString } = this.props;
     return (
       <div>
         <div className="p-Sql-ConnectionInformation-input-wrapper">
           <div className="p-Sql-ConnectionInformation-input-text">{connectionString}</div>
         </div>
-        <div className="p-Sql-ConnectionInformation-edit-button"></div>
+        <div className="p-Sql-ConnectionInformation-edit-button" onClick={() => this.startEditing() }></div>
       </div>
     )
+  }
+
+  render() {
+    const { editing } = this.state
+    if (editing) {
+      return <p>Editing</p>
+    } else {
+      return this.renderDisplaying()
+    }
   }
 }
