d99ecd42f4c70bbfeb7125ff42ce237d8e1435b2
Pascal Bugnion <pascal@bugnion.org>
2019-02-05 07:49:35 +0000

    Additional tests for row serialization

--- jupyterlab_sql/tests/handlers/test_serializer.py
+++ jupyterlab_sql/tests/handlers/test_serializer.py
@@ -1,8 +1,23 @@
 
+import uuid
+
+import pytest
+
 from jupyterlab_sql.handlers.serializer import make_row_serializable
 
-def test_make_row_serializable():
-    inp = (1, 'hello')
-    out = (1, 'hello')
-    assert make_row_serializable(inp) == out
-     
+
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        ((1, 'hello'),),
+        ((1, {'some': 'dict'}),),
+        ((1, {'some': ['array', 'in', 'dict']}),)
+    ])
+def test_make_row_serializable_unchanged(test_input):
+    assert make_row_serializable(test_input) == test_input
+
+
+def test_serialize_uuid():
+    uuid_str = '6e1d16e3-ca00-4e96-9735-95bf92a8c46c'
+    row = (1, uuid.UUID(uuid_str))
+    assert make_row_serializable(row) == (1, uuid_str)
