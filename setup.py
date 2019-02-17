dedb47cf92bc184181e736ad262be43a234ba20f
Pascal Bugnion <pascal@bugnion.org>
2019-02-17 15:06:52 +0000

    Use version from setuptools_scm

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setuptools.setup(
     name="jupyterlab_sql",
     version="0.1.0",
     packages=setuptools.find_packages(),
+    setup_requires=['setuptools_scm'],
+    use_scm_version={"version_scheme": "post-release"},
     install_requires=[
         "jupyterlab",
         "sqlalchemy"
