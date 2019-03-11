99c67286f5107e577166addc176f83efa0cbf493
Pascal Bugnion <pascal@bugnion.org>
2019-03-11 13:57:53 +0000

    Add context menu to response grid

--- src/responseGrid.ts
+++ src/responseGrid.ts
@@ -1,6 +1,8 @@
-import { Widget } from '@phosphor/widgets';
+import { Menu, Widget } from '@phosphor/widgets';
 
 import { DataGrid, DataModel, TextRenderer, CellRenderer } from '@phosphor/datagrid'
+
+import { CommandRegistry } from '@phosphor/commands';
 
 import { DataGridExtensions } from './services'
 
@@ -27,16 +29,30 @@ export class ResponseGrid {
     this._selectionManager.selectionChanged.connect(() => {
       this._updateRenderers()
     })
+
+    const menu = this._createContextMenu();
+
+    DataGridExtensions.addMouseEventListener(
+      'contextmenu',
+      this._grid,
+      event => {
+        const { row, column, rawEvent } = event;
+        this._updateSelection(row, column)
+        if (this._isInBody(row, column)) {
+          menu.open(rawEvent.clientX, rawEvent.clientY)
+          rawEvent.preventDefault()
+        }
+      }
+    )
   }
 
-  _onClick(event: DataGridExtensions.MouseEvent) {
+  private _onClick(event: DataGridExtensions.GridMouseEvent) {
     const { row, column } = event;
-    if (
-      row.section === 'row' &&
-        column.section === 'column' &&
-        row.index !== null &&
-        column.index !== null
-    ) {
+    this._updateSelection(row, column)
+  }
+
+  private _updateSelection(row: DataGridExtensions.Row, column: DataGridExtensions.Column) {
+    if (this._isInBody(row, column)) {
       this._selectionManager.selection = {
         rowIndex: row.index,
         columnIndex: column.index
@@ -46,12 +62,21 @@ export class ResponseGrid {
     }
   }
 
-  _updateRenderers(): void {
+  private _isInBody(row: DataGridExtensions.Row, column: DataGridExtensions.Column) {
+    return (
+      row.section === 'row' &&
+        column.section === 'column' &&
+        row.index !== null &&
+        column.index !== null
+    )
+  }
+
+  private _updateRenderers(): void {
     const renderer = this._textRendererForSelection(this._selectionManager.selection)
     this._grid.cellRenderers.set('body', {}, renderer)
   }
 
-  _textRendererForSelection(selectedCell: DataGridExtensions.BodyCellIndex | null): CellRenderer {
+  private _textRendererForSelection(selectedCell: DataGridExtensions.BodyCellIndex | null): CellRenderer {
     let backgroundColor;
     let textColor;
     if (selectedCell === null) {
@@ -80,6 +105,17 @@ export class ResponseGrid {
 
   get widget(): Widget {
     return this._grid
+  }
+
+  private _createContextMenu(): Menu {
+    const commands = new CommandRegistry()
+    commands.addCommand('hello-world', {
+      label: 'hello',
+      execute: () => { console.log('hi') }
+    })
+    const menu = new Menu({ commands })
+    menu.addItem({ command: 'hello-world' })
+    return menu
   }
 
   private readonly _grid: DataGrid;
