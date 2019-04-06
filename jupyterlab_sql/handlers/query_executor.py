7edf2449af5594b3e9da7674b5396f1692cedacf
Pascal Bugnion <pascal@bugnion.org>
2019-04-06 11:24:35 +0100

    Allow creating sqllite engines

--- jupyterlab_sql/handlers/query_executor.py
+++ jupyterlab_sql/handlers/query_executor.py
@@ -1,7 +1,10 @@
 
 from sqlalchemy import create_engine
+import sqlalchemy.engine.url
+from sqlalchemy.pool import StaticPool
 
 from .serializer import make_row_serializable
+from .cache import Cache
 
 
 class QueryResult:
@@ -21,8 +24,23 @@ class QueryResult:
 
 
 class QueryExecutor:
+    def __init__(self):
+        self._sqlite_engine_cache = Cache()
+
     def execute_query(self, connection_string, query):
-        engine = create_engine(connection_string)
+        backend = sqlalchemy.engine.url.make_url(
+            connection_string
+        ).get_backend_name()
+        if backend == 'sqlite':
+            engine_builder = lambda: create_engine(
+                connection_string,
+                connect_args={'check_same_thread': False},
+                poolclass=StaticPool
+            )
+            engine = self._sqlite_engine_cache.get_or_set(
+                connection_string, engine_builder)
+        else:
+            engine = create_engine(connection_string)
         result = self._execute_with_engine(engine, query)
         return QueryResult.from_sqlalchemy_result(result)
 
