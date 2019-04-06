7bc939c9af89726f009e74d2bb86f1c1b82e2be9
Pascal Bugnion <pascal@bugnion.org>
2019-04-06 13:47:14 +0100

    Additional tests for connection url

--- jupyterlab_sql/tests/handlers/test_connection_url.py
+++ jupyterlab_sql/tests/handlers/test_connection_url.py
@@ -5,7 +5,13 @@ import jupyterlab_sql.handlers.connection_url as connection_url
 
 @pytest.mark.parametrize(
     "url",
-    ["sqlite://"]
+    [
+        "sqlite://",
+        "sqlite:///test.db",
+        "sqlite:////test.db",
+        "sqlite+pysqlcipher://:testing@/foo.db?cipher=aes-256-cfb&kdf_iter=64000",
+        "sqlite+pysqlite:///file.db"
+    ]
 )
 def test_sqlite(url):
     assert connection_url.is_sqlite(url)
@@ -13,7 +19,10 @@ def test_sqlite(url):
 
 @pytest.mark.parametrize(
     "url",
-    ["postgres://localhost:5432/postgres"]
+    [
+        "postgres://localhost:5432/postgres",
+        "mysql://localhost",
+    ]
 )
 def test_not_sqlite(url):
     assert not connection_url.is_sqlite(url)
