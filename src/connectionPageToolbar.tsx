e4dd68709c65ef846cf1fbf94b3ba1236cafacac
Pascal Bugnion <pascal@bugnion.org>
2019-04-08 11:10:49 +0100

    Initial implementation of connection page toolbar

--- src/connectionPageToolbar.tsx
+++ src/connectionPageToolbar.tsx
@@ -4,6 +4,8 @@ import { ISignal, Signal } from '@phosphor/signaling';
 
 import { VDomModel, VDomRenderer } from '@jupyterlab/apputils';
 
+import classNames from 'classNames';
+
 export interface IConnectionPageToolbarModel {
   readonly connectionString: string;
   readonly connect: ISignal<this, string>;
@@ -39,11 +41,107 @@ class ToolbarContainer extends VDomRenderer<ConnectionPageToolbarModel> {
     if (!this.model) {
       return null;
     } else {
+      const connectionString = this.model.connectionString
       return (
         <div className="p-Sql-Toolbar">
-          hello toolbar
+          <ConnectionInformationEdit
+            connectionString={connectionString}
+            onFinishEdit={console.log}
+          />
         </div>
       );
     }
+  }
+}
+
+
+class ConnectionInformationEdit extends React.Component<
+  ConnectionInformationEdit.Props,
+  ConnectionInformationEdit.State
+> {
+  constructor(props: ConnectionInformationEdit.Props) {
+    super(props);
+    this.state = {
+      value: props.connectionString,
+      focused: false
+    };
+  }
+
+  private inputRef = React.createRef<HTMLInputElement>();
+
+  onKeyDown(event: React.KeyboardEvent<HTMLInputElement>): void {
+    if (event.key === 'Enter') {
+      this.inputRef.current!.blur();
+    } else if (event.keyCode === 27) {
+      // ESC key
+      this.cancel();
+    }
+  }
+
+  onChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ value: event.target.value });
+  }
+
+  start() {
+    this.setState({
+      focused: true,
+      value: this.props.connectionString
+    });
+  }
+
+  finish() {
+    this.props.onFinishEdit(this.state.value);
+    this.setState({
+      focused: false
+    });
+  }
+
+  cancel() {
+    this.setState({ value: this.props.connectionString });
+  }
+
+  onInputFocus() {
+    this.start();
+  }
+
+  onInputBlur() {
+    this.finish();
+  }
+
+  componentDidMount() {
+    this.inputRef.current!.focus();
+  }
+
+  render() {
+    const { value, focused } = this.state;
+    const inputWrapperClass = classNames(
+      'p-Sql-ConnectionInformation-input-wrapper',
+      { 'p-mod-focused': focused }
+    );
+    return (
+      <div className={inputWrapperClass}>
+        <input
+          className="p-Sql-ConnectionInformation-text p-Sql-ConnectionInformation-input"
+          value={value}
+          ref={this.inputRef}
+          onChange={event => this.onChange(event)}
+          onKeyDown={event => this.onKeyDown(event)}
+          onBlur={() => this.onInputBlur()}
+          onFocus={() => this.onInputFocus()}
+        />
+      </div>
+    );
+  }
+}
+
+namespace ConnectionInformationEdit {
+  export interface Props {
+    connectionString: string;
+    onFinishEdit: (newConnectionString: string) => void;
+  }
+
+  export interface State {
+    value: string;
+    focused: boolean;
   }
 }
