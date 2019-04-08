a7f5033a8def20544af0ac50776fd515b3cb39f3
Pascal Bugnion <pascal@bugnion.org>
2019-04-08 16:56:43 +0100

    React to response from API

--- src/databaseSummary/index.ts
+++ src/databaseSummary/index.ts
@@ -1,5 +1,7 @@
 
-import { BoxPanel } from '@phosphor/widgets';
+import { Widget, BoxPanel, SingletonLayout, LayoutItem } from '@phosphor/widgets';
+import { Message } from '@phosphor/messaging';
+import { Api } from '../api'
 
 namespace DatabaseSummaryPage {
   export interface IOptions {
@@ -10,10 +12,60 @@ namespace DatabaseSummaryPage {
 export class DatabaseSummaryPage extends BoxPanel {
   constructor(options: DatabaseSummaryPage.IOptions) {
     super();
+    this.responseWidget = new ResponseWidget()
+    this.responseWidget.setResponse("loading")
+    this.getStructure()
+    this.addWidget(this.responseWidget);
+    BoxPanel.setStretch(this.responseWidget, 1)
+  }
+
+  async getStructure(): Promise<void> {
+    const response = await Api.getStructure()
+    this.responseWidget.setResponse(response)
+  }
+
+  private readonly responseWidget: ResponseWidget
+}
+
+class ResponseWidget extends Widget {
+  constructor() {
+    super();
+    this.layout = new SingletonLayout();
+  }
+
+  onResize(_: Message) {
+    if (this._item) {
+      this._fitCurrentWidget()
+    }
+  }
+
+  setResponse(response: any) {
+    const widget = new TextResponseWidget(JSON.stringify(response));
+    this._setCurrentWidget(widget);
+  }
+
+  private _setCurrentWidget(widget: Widget) {
+    this.layout.widget = widget;
+    this._item = new LayoutItem(this.layout.widget);
+    this._fitCurrentWidget();
+  }
+
+  private _fitCurrentWidget() {
+    this._item.update(0, 0, this.node.offsetWidth, this.node.offsetHeight);
+  }
+
+  readonly layout: SingletonLayout;
+  private _item: LayoutItem;
+}
+
+
+class TextResponseWidget extends Widget {
+  constructor(message: string) {
+    super();
     const element = document.createElement('div');
     const pre = document.createElement('pre');
-    pre.innerHTML = 'hello database summary'
-    element.appendChild(pre)
+    pre.innerHTML = message;
+    element.appendChild(pre);
     this.node.appendChild(element);
   }
 }
