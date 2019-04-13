7439467a7d50177eaeff49b59ac89233d2420049
Pascal Bugnion <pascal@bugnion.org>
2019-04-13 14:18:18 +0100

    Test that the sub-widget is resized

--- __tests__/src/components/SingletonPanel.ts
+++ __tests__/src/components/SingletonPanel.ts
@@ -17,11 +17,23 @@ namespace Fixtures {
 
 describe('SingletonPanel', () => {
   it('allow setting a widget', () => {
-    const w = new Fixtures.TestWidget()
     const panel = new SingletonPanel()
-    panel.widget = w
+    panel.widget = new Fixtures.TestWidget()
     const widgetNode = panel.node.querySelector('#test-widget-id')
     expect(widgetNode).toBeDefined();
     expect(widgetNode.innerHTML).toEqual('some text');
+  })
+
+  it('resize widgets based on its own size', () => {
+    const panel = new SingletonPanel();
+    // Set the node as having a fixed size (since jsdom does no layout)
+    Object.defineProperties(panel.node, {
+      offsetWidth: { get: () => 200 },
+      offsetHeight: { get: () => 400 }
+    })
+    const widget = new Fixtures.TestWidget();
+    const resizeSpy = jest.spyOn(widget, 'processMessage')
+    panel.widget = widget;
+    expect(resizeSpy).toBeCalledWith(new Widget.ResizeMessage(200, 400));
   })
 })
