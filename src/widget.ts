4e48e8c5b7c26e38c5c0b65fee82284106c72b3f
Pascal Bugnion <pascal@bugnion.org>
2019-04-13 15:15:40 +0100

    Reuse singleton panel in main widget

--- src/widget.ts
+++ src/widget.ts
@@ -1,8 +1,6 @@
-import { SingletonLayout, Widget, LayoutItem } from '@phosphor/widgets';
-
-import { Message } from '@phosphor/messaging';
-
 import { IEditorFactoryService } from '@jupyterlab/codeeditor';
+
+import { SingletonPanel } from './components';
 
 import { QueryPage } from './queryPage';
 
@@ -20,15 +18,13 @@ namespace JupyterLabSqlWidget {
 }
 
 
-export class JupyterLabSqlWidget extends Widget {
+export class JupyterLabSqlWidget extends SingletonPanel {
   constructor(editorFactory: IEditorFactoryService, options: JupyterLabSqlWidget.IOptions) {
     super();
     this.name = options.name;
     this.id = 'jupyterlab-sql';
     this.title.label = 'SQL';
     this.title.closable = true;
-
-    this.layout = new SingletonLayout();
 
     this.editorFactory = editorFactory;
     const widget = new ConnectionPage({
@@ -42,31 +38,13 @@ export class JupyterLabSqlWidget extends Widget {
           initialSqlStatement: 'select * from t'
         }
         const widget = new QueryPage(editorFactory, options)
-        this._setCurrentWidget(widget)
+        this.widget = widget
       })
-      this._setCurrentWidget(widget)
+      this.widget = widget
     })
-    this._setCurrentWidget(widget);
-  }
-
-  private _setCurrentWidget(widget: Widget) {
-    this.layout.widget = widget;
-    this._item = new LayoutItem(this.layout.widget);
-    this._fitCurrentWidget();
-  }
-
-  private _fitCurrentWidget() {
-    this._item.update(0, 0, this.node.offsetWidth, this.node.offsetHeight);
-  }
-
-  onResize(_: Message) {
-    if (this._item) {
-      this._fitCurrentWidget();
-    }
+    this.widget = widget
   }
 
   readonly editorFactory: IEditorFactoryService;
   readonly name: string;
-  readonly layout: SingletonLayout;
-  private _item: LayoutItem;
 }
