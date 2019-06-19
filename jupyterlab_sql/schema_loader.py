a36710c9a3ee36bb3cfb55b5e0b7aa0ab8b39404
Pascal Bugnion <pascal@bugnion.org>
2019-06-19 08:08:24 +0100

    Use pathlib for loading schemas

--- jupyterlab_sql/schema_loader.py
+++ jupyterlab_sql/schema_loader.py
@@ -1,8 +1,13 @@
-from pkg_resources import resource_string
 import json
+from pathlib import Path
+from pkg_resources import resource_string
+
 from jsonschema import Draft7Validator as Validator
 
 
+SCHEMA_PATH = Path("schemas")
+
+
 def load(name):
-    schema_string = resource_string(__name__, f"schemas/{name}")
+    schema_string = resource_string(__name__, SCHEMA_PATH / name)
     return Validator(json.loads(schema_string))
