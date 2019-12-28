8c2be7eedece190dc2a4e97cf2c3f74633193a26
Pascal Bugnion <pascal@bugnion.org>
2019-12-28 15:28:40 +0000

    Allow checking if database is mysql

--- jupyterlab_sql/connection_url.py
+++ jupyterlab_sql/connection_url.py
@@ -4,3 +4,8 @@ import sqlalchemy.engine.url
 def is_sqlite(url):
     backend = sqlalchemy.engine.url.make_url(url).get_backend_name()
     return backend == "sqlite"
+
+
+def is_mysql(url):
+    backend = sqlalchemy.engine.url.make_url(url).get_backend_name()
+    return backend == "mysql"
