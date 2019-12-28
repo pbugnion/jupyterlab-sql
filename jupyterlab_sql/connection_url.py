88212f64f65729136cecc2641e9db93543d4df04
Pascal Bugnion <pascal@bugnion.org>
2019-12-28 15:39:49 +0000

    Add functionality to check if URL has a database

--- jupyterlab_sql/connection_url.py
+++ jupyterlab_sql/connection_url.py
@@ -2,10 +2,21 @@ import sqlalchemy.engine.url
 
 
 def is_sqlite(url):
-    backend = sqlalchemy.engine.url.make_url(url).get_backend_name()
+    backend = _to_sqlalchemy_url(url).get_backend_name()
     return backend == "sqlite"
 
 
 def is_mysql(url):
-    backend = sqlalchemy.engine.url.make_url(url).get_backend_name()
+    backend = _to_sqlalchemy_url(url).get_backend_name()
     return backend == "mysql"
+
+
+def has_database(url):
+    database = _to_sqlalchemy_url(url).database
+    # database is either None or an empty string, depending on
+    # whether the URL contains a trailing slash.
+    return bool(database)
+
+
+def _to_sqlalchemy_url(url):
+    return sqlalchemy.engine.url.make_url(url)
